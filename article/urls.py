e4b9ee5e902ed7e40b96af26d21af6d8aeaf6712
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-07-31 15:46:21 +0530

    Reverted last commit.

--- article/urls.py
+++ article/urls.py
@@ -6,7 +6,7 @@ from location.views import LocationDetail
 urlpatterns = patterns('article.views',
     url(r'^authors/(?P<slug>.+)/$', ArticleList.as_view(), name='author-detail'),
     url(r'^articles/$', ArticleList.as_view(), name='article-list'),
-    url(r'^articles/(?P<slug>.+)/?$', ArticleDetail.as_view(), name='article-detail'),
+    url(r'^articles/(?P<slug>.+)/$', ArticleDetail.as_view(), name='article-detail'),
     url(r'^locations/(?P<slug>.+)/$', LocationDetail.as_view(), name='location-detail'),
     url(r'^archive/(?P<year>\d{4})/(?P<month>\d+)/$', ArchiveDetail.as_view(), name='archive-detail'),
 )
