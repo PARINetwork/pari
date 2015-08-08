948dd69f714dfb60f9ef7cd2bb567255fcdbed3d
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-08-08 22:19:55 +0530

    Site Title from the settings.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings as django_settings
+from django.utils.translation import ugettext_lazy as _
 
 def settings(request):
     if not getattr(django_settings, "SOCIAL", None):
@@ -8,4 +9,5 @@ def settings(request):
         "SOCIAL_TWITTER": django_settings.SOCIAL.get("TWITTER", ""),
         "SOCIAL_GITHUB_REPO": django_settings.SOCIAL.get("GITHUB_REPO", ""),
         "GOOGLE_ANALYTICS_ID": django_settings.SOCIAL.get("GOOGLE_ANALYTICS_ID", ""),
+        "SITE_TITLE": _("People's Archive of Rural India")
     }
