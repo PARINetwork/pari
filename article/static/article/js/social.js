9c7421bb34abd62e00bd0db8f6d9bd17ecb44f34
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-08-09 14:49:13 +0530

    URL changes from http to https.

--- article/static/article/js/social.js
+++ article/static/article/js/social.js
@@ -1,12 +1,12 @@
 $(function(){
     var socialBar = $('.social');
-    $.get('http://graph.facebook.com/?id=' + socialBar.data('url'), function(data) {
+    $.get('https://graph.facebook.com/?id=' + socialBar.data('url'), function(data) {
         $('.facebook .count', socialBar).html(data['shares']);
     })
     .fail(function() {
         $('.facebook .count', socialBar).html('0');
     });
-    $.getJSON('http://urls.api.twitter.com/1/urls/count.json?callback=?', {'url': socialBar.data('url')}, function(data) {
+    $.getJSON('https://urls.api.twitter.com/1/urls/count.json?callback=?', {'url': socialBar.data('url')}, function(data) {
         $('.twitter .count', socialBar).html(data['count']);
     })
     .fail(function() {
