cbd8127eb031443f07a54f5923dd9e5a85920461
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-08-13 22:25:30 +0530

    Remove special characters and extra spaces from the slug.

--- article/static/article/js/slug.js
+++ article/static/article/js/slug.js
@@ -1,3 +1,3 @@
 function cleanForSlug(val, useURLify) {
-    return val.replace(/[^A-Za-z0-9\0080-\uFFFF\-\s]/g, '').replace(/\s+/g, '-').toLowerCase();
+    return val.replace(/[^A-Za-z0-9\0080-\uFFFF\-\s]/g, '').trim().replace(/[\!\@\#\$\%\^\&\*\(\)\{\}\:\;\"\'\,\.\/\?\+\-\<\>]/g,'').replace(/\s+/g, '-').toLowerCase();
 }
