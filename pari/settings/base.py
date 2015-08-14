228fe9dfb384303e25c40e03a19e2d0f954252c9
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-08-14 19:18:56 +0530

    Update the timezone to point to India.

--- pari/settings/base.py
+++ pari/settings/base.py
@@ -118,7 +118,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
