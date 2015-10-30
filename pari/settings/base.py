110065ede287ea04761ccae36c8a334a6a689035
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-10-30 12:33:53 +0530

    Fixes #1. Override the default wagtail admin template to list author/photographer.

--- pari/settings/base.py
+++ pari/settings/base.py
@@ -220,3 +220,28 @@ LANGUAGES = (
 SITE_ID = 1
 
 SITE_TITLE = ""
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(PROJECT_ROOT, 'templates'),
+        ],
+        'OPTIONS': {
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'context_processors': (
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages"
+            ),
+        },
+    },
+]
