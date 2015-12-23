fed49ad32f19561866580a30e65d95cf55b6257a
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2015-12-23 21:14:58 +0530

    Category display in the admin.

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Category
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+admin.site.register(Category, CategoryAdmin)
