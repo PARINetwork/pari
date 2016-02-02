f49f71082ce7a9eaa5991a8f29bdb5a2f6679e53
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-02-02 21:59:28 +0530

    Remove the URL count numbers now that twitter stopped reporting them.

--- article/static/article/js/social.js
+++ article/static/article/js/social.js
@@ -1,17 +1,4 @@
 $(function(){
-    var socialBar = $('.social[data-url]');
-    $.get('https://graph.facebook.com/?id=' + socialBar.data('url'), function(data) {
-        $('.facebook .count', socialBar).html(data['shares']);
-    })
-    .fail(function() {
-        $('.facebook .count', socialBar).html('0');
-    });
-    $.getJSON('https://urls.api.twitter.com/1/urls/count.json?callback=?', {'url': socialBar.data('url')}, function(data) {
-        $('.twitter .count', socialBar).html(data['count']);
-    })
-    .fail(function() {
-        $('.twitter .count', socialBar).html('0');
-    });
     if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
 	// Mobile device.
 	$(".hidden.whatsapp").removeClass("hidden")
