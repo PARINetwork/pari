d0eae862d31025c42a51f3bf903f9a51945906e0
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-02-02 21:58:31 +0530

    Display the latest face in the list view.

--- face/views.py
+++ face/views.py
@@ -14,7 +14,7 @@ class FaceList(ListView):
             'FROM face_face JOIN location_location ON '
             'face_face.location_id=location_location.id '
             'JOIN wagtailcore_page ON wagtailcore_page.id=face_face.page_ptr_id '
-            'ORDER BY substr(district, 1, 1)')
+            'ORDER BY substr(district, 1, 1), first_published_at DESC')
         return qs
 
 
@@ -25,7 +25,9 @@ class FaceDetail(ListView):
 
     def get_queryset(self):
         alphabet = self.kwargs['alphabet']
-        return Face.objects.live().filter(location__district__istartswith=alphabet)
+        return Face.objects.live().filter(
+            location__district__istartswith=alphabet
+        ).order_by('-first_published_at')
 
     def get_context_data(self):
         context = super(FaceDetail, self).get_context_data()
