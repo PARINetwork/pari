e35ed3eb2b5a54047afd48629e87affbead01b97
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-05-28 17:45:37 +0530

    Handle the image alignment case.

--- article/static/article/js/align.js
+++ article/static/article/js/align.js
@@ -1,15 +1,20 @@
 // Temporary workaround until wagtail 1.5 is released
 $(function() {
     $(".richtext").on("click", function() {
-	var $this = $(this);
-	$(".hallotoolbar .hallojustify button[title]").on("click", function() {
-	    var selection = window.getSelection();
-	    var blockDisplayParent = $(selection.baseNode).parents("p");
-	    var alignment = $(this).attr("title").toLowerCase();
-	    $(blockDisplayParent).removeClass("center left right justify");
-	    $(blockDisplayParent).addClass(alignment);
-	    $this.trigger("hallomodified", [{content: $this.html()}]);
-	    $this.addClass("isModified");
-	});
+	      var $this = $(this);
+	      $(".hallotoolbar .hallojustify button[title]").on("click", function() {
+	          var selection = window.getSelection();
+	          var block;
+            if (selection.baseNode.tagName.toLowerCase() === "p") {
+                block = selection.baseNode;
+            } else {
+                block = $(selection.baseNode).parents("p");
+            }
+	          var alignment = $(this).attr("title").toLowerCase();
+	          $(block).removeClass("center left right justify");
+	          $(block).addClass(alignment);
+	          $this.trigger("hallomodified", [{content: $this.html()}]);
+	          $this.addClass("isModified");
+	      });
     });
 });
