beb67b72c5d5cb4fbe4ca1118a8d78a55d9f9586
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-07-23 18:10:42 +0530

    Map fixes display for async google loading.

--- article/static/article/js/map.js
+++ article/static/article/js/map.js
@@ -4,15 +4,18 @@ $(function() {
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 7
         });
-    }
-    var lat = $('.map-view').data("lat").toString().split(","),
-        lng = $('.map-view').data("long").toString().split(",");
 
-    for (var i = 0; i < lat.length; i++) {
-        map.setCenter([lat[i], lng[i]]);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: {lat: lat[i], lng: lng[i]}
-        });
+        var lat = $('.map-view').data("lat").toString().split(","),
+            lng = $('.map-view').data("long").toString().split(",");
+
+
+        for (var i = 0; i < lat.length; i++) {
+            var pos = {lat: parseFloat(lat[i]), lng: parseFloat(lng[i])};
+            map.setCenter(pos);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: pos
+            });
+        }
     }
 });
