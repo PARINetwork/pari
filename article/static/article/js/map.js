f59d1616c4a672d93cfd2eaf659dc3db6b9e331b
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-07-23 17:48:00 +0530

    OSM -> Google maps for article page.

--- article/static/article/js/map.js
+++ article/static/article/js/map.js
@@ -1,24 +1,18 @@
-$(function () {
-    try {
-        var map = L.map('map');
-    }
-     catch(e){
-        return true;
+$(function() {
+    var map;
+    window.initMap = function() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7
+        });
     }
     var lat = $('.map-view').data("lat").toString().split(","),
-        long = $('.map-view').data("long").toString().split(",");
-
-    L.tileLayer('https://otile{s}-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
-	subdomains: '1234',
-        maxZoom: 7,
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors. '
-	    + 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
-	    + '<img src="https://developer.mapquest.com/sites/default/files/mapquest/osm/mq_logo.png">'
-    }).addTo(map);
+        lng = $('.map-view').data("long").toString().split(",");
 
     for (var i = 0; i < lat.length; i++) {
-        map.setView([lat[i], long[i]], 6);
-        L.marker([lat[i], long[i]]).addTo(map);
+        map.setCenter([lat[i], lng[i]]);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: {lat: lat[i], lng: lng[i]}
+        });
     }
-
 });
