0d65a926460538106bbf157d2532112cdefb8ae0
Nimesh <nimesh@thoughtworks.com>
2017-02-20 12:38:15 +0530

    [Nimesh/lokesh]#28 solved migration conflicts

---
+++ core/migrations/0008_auto_20170220_1223.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailimages.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_auto_20170217_1247'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='affiximage',
+            name='photographer',
+        ),
+        migrations.AddField(
+            model_name='affiximage',
+            name='camera',
+            field=models.CharField(max_length=50, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='affiximage',
+            name='date',
+            field=models.DateTimeField(null=True, blank=True),
+        ),
+    ]
