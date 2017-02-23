3385b30d70c1f3d8bfa7fc075b4f60d0b23d39f1
Deepthi R <deepthirera@gmail.com>
2017-02-23 11:18:19 +0530

    [Deepthi] Misc -Fix for vagrant

--- pari/settings/dev.py
+++ pari/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+ALLOWED_HOSTS = [ '0.0.0.0', '127.0.0.1', 'localhost' ]
 
 try:
     from .local import *
