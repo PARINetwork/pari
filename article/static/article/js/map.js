958f34e9a7ce992c8b4c5c4047347e4be92c2814
Lokesh Sharma <slokesh@thoughtworks.com>
2017-03-20 15:12:08 +0530

    #56 [Lokesh/Vidhya] Added maps with multiple markers and infobox for the marker

--- article/static/article/js/map.js
+++ article/static/article/js/map.js
@@ -1,21 +1,31 @@
-$(function() {
-    var map;
-    window.initMap = function() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 7
+var map;
+initMap=function(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7
+    });
+
+    var lat = $('.map-view').data("lat").toString().split(","),
+        lng = $('.map-view').data("long").toString().split(",");
+        content = $('.map-view').data("content").toString().split(",");
+
+    for (var i = 0; i < lat.length; i++) {
+        var pos = {lat: parseFloat(lat[i]), lng: parseFloat(lng[i])};
+        map.setCenter(pos);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: pos,
         });
-
-        var lat = $('.map-view').data("lat").toString().split(","),
-            lng = $('.map-view').data("long").toString().split(",");
-
-
-        for (var i = 0; i < lat.length; i++) {
-            var pos = {lat: parseFloat(lat[i]), lng: parseFloat(lng[i])};
-            map.setCenter(pos);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: pos
-            });
-        }
+        var infowindow = new google.maps.InfoWindow({
+        });
+      google.maps.event.addListener(marker,'mouseover', (function(marker,content,infowindow){
+            return function() {
+                infowindow.setContent(content);
+                infowindow.open(map,marker);
+            };
+        })(marker,content[i],infowindow));
     }
+};
+
+$(function() {
+    initMap();
 });
