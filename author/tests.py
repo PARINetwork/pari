5ccbdbf86fdd5055d36de77987576780e364e078
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-03-21 12:58:08 +0530

    Add basic model test
    To execute tests: python manage.py test --keepdb

--- author/tests.py
+++ author/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+import factory
+
+from author.models import Author
+
+
+class AuthorFactory(factory.Factory):
+    class Meta:
+        model = Author
+
+    name = 'An author'
+
+
+class AuthorTests(TestCase):
+    def test_string_representaion(self):
+        assert str(AuthorFactory()) == 'An author'
