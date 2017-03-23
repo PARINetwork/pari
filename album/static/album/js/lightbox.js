f92200d2edf7d383befd9743febe8f7fb32c9fa1
Lokesh Sharma <slokesh@thoughtworks.com>
2017-03-23 18:12:55 +0530

    #61 [Lok/Dee] Authors slide as part of slideshow with menu wip

--- album/static/album/js/lightbox.js
+++ album/static/album/js/lightbox.js
@@ -1,6 +1,6 @@
 var Album = {
     init: function() {
-        this._initSoundCloudWidget();
+        // this._initSoundCloudWidget();
         this._initControls();
     },
 
@@ -10,23 +10,23 @@ var Album = {
         this._popup = $('.popup-gallery').magnificPopup({
 
             items: itemsData,
-            
+
             tLoading: 'Loading image #%curr%...',
 
             mainClass: 'mfp-album-popup',
-            
+
             gallery: {
                 enabled: true,
                 navigateByImgClick: true,
                 preload: [0, 2]
             },
-                    
+
             image: {
                 cursor: null,
                 tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 
                 titleSrc: $.proxy(function (item) {
-                    // if(item.data.show_title) {
+                    if(item.data.show_title) {
                         // isCommentsAllowed = item.el.attr('data-allowcomments') == "True" ? true : false;
                         // var slideshow = this._popup.data('slideshow');
                         // var icon = slideshow ? "pause" : "play";
@@ -49,7 +49,7 @@ var Album = {
                         returnedHTMLElement += '</div>' +
                             '</div>' +
                             '</div>';
-                    // }
+                    }
                     return returnedHTMLElement
                 }, this),
 
@@ -61,14 +61,15 @@ var Album = {
             callbacks: {
                 updateStatus: $.proxy(function () {
                     this._initImage();
-                    this._initAudio();
+                    // this._initAudio();
                 }, this),
                 markupParse: function(template, values, item) {
                     $(template).find('a.open-in-new-tab').attr('href', values.src);
                 },
                 close: $.proxy(function () {
-                    this._stopWidget();
-                    this._popup.removeData('slideshow');
+                    // this._stopWidget();
+                    this._popup.removeData('slide-show');
+                    clearInterval(this._popup.data('slide-show-timer'));
                 }, this),
 
 		open: function() {
@@ -99,63 +100,64 @@ var Album = {
         });
     },
 
-    _initSoundCloudWidget: function() {
-        SC.initialize({
-            client_id: "d129911dd3c35ec537c30a06990bd902"
-        });
-    },
+    // _initSoundCloudWidget: function() {
+    //     SC.initialize({
+    //         client_id: "d129911dd3c35ec537c30a06990bd902"
+    //     });
+    // },
 
-    _player: null,
-    _reloadWidget: function(audio, autoplay) {
-	var $this = this;
-        SC.stream("/tracks/" + audio).then(function(player) {
-	    $this._player = player;
-	    player.play();
-	    player.on("finish", function() {
-		$this._onSoundFinish();
-	    });
-	});
-    },
+    // _player: null,
+    // _reloadWidget: function(audio, autoplay) {
+    // var $this = this;
+    //     SC.stream("/tracks/" + audio).then(function(player) {
+	 //    $this._player = player;
+	 //    player.play();
+	 //    player.on("finish", function() {
+		// $this._onSoundFinish();
+	 //    });
+    // });
+    // },
 
-    _toggleWidget: function() {
-	this._player.toggle();
-	this._togglePlayButton();
-    },
+    // _toggleWidget: function() {
+    // this._player.toggle();
+    // this._togglePlayButton();
+    // },
 
-    _updateSlideshowButtonIcon: function () {
-        var slideshow = this._popup.data('slideshow');
-        var slideshowButton = $('.btn-slideshow i');
-        if(slideshow) { 
-            slideshowButton.addClass('fa-pause');
-            slideshowButton.removeClass('fa-play');
-        } else {
-            slideshowButton.addClass('fa-play');
-            slideshowButton.removeClass('fa-pause');
-        }
-    },
+    // _updateSlideshowButtonIcon: function () {
+    //     var slideshow = this._popup.data('slideshow');
+    //     var slideshowButton = $('.btn-slideshow i');
+    //     if(slideshow) {
+    //         slideshowButton.addClass('fa-pause');
+    //         slideshowButton.removeClass('fa-play');
+    //     } else {
+    //         slideshowButton.addClass('fa-play');
+    //         slideshowButton.removeClass('fa-pause');
+    //     }
+    // },
 
-    _stopWidget: function() {
-	if (this._player) {
-	    this._player.seek(0);
-	    this._player.pause();
-	}
-    },
+    // _stopWidget: function() {
+    // if (this._player) {
+	 //    this._player.seek(0);
+	 //    this._player.pause();
+    // }
+    // },
 
-    _onSoundFinish: function() {
-        var slideshow = this._popup.data('slideshow');
-        if(slideshow) {
-            var magnificPopup = $.magnificPopup.instance;
-            magnificPopup.next();
-            return;
-        }
-        this._initPlayButton();
-    },
+    // _onSoundFinish: function() {
+    //     var slideshow = this._popup.data('slideshow');
+    //     if(slideshow) {
+    //         var magnificPopup = $.magnificPopup.instance;
+    //         magnificPopup.next();
+    //         return;
+    //     }
+    //     this._initPlayButton();
+    // },
 
     _initControls: function() {
         $('.grid-container').click($.proxy(function (element) {
-            var slug = $(element.currentTarget).data('slug');
-            $.get("/albums/"+slug+".json/", $.proxy(function(response) {
-                this._initPopup(response.data);
+            // var slug = $(element.currentTarget).data('slug');
+            // $.get("/albums/"+slug+".json/", $.proxy(function(response) {
+                var slidesWithAuthor = this._constructAuthorItem(this._dummy());
+                this._initPopup(slidesWithAuthor);
                 this._popup.magnificPopup('open');
                 this._playSlideShow();
                 $('.mfp-container').addClass('mfp-container-fullscreen');
@@ -163,7 +165,19 @@ var Album = {
                    this._handleSlideShow();
                 }, this));
             }, this));
-        }, this));
+        // }, this));
+    },
+
+    _constructAuthorItem: function(itemsJson){
+        var authorTmpl = $.templates("#authorTmpl");
+        var authors = authorTmpl.render(itemsJson['authors']);
+        var authorSrc = $($("#author-template").html()).find("#author").append(authors).parent().html();
+        var authorItem = {
+            src: authorSrc,
+            type: 'inline',
+            show_title: false
+        };
+        return itemsJson['slides'].concat(authorItem);
     },
 
     _handleSlideShow: function () {
@@ -178,8 +192,14 @@ var Album = {
     _playSlideShow: function () {
         $('.slide-show').addClass('fa-pause').removeClass('fa-play');
         this._popup.data('slide_show', 'true');
+        var _this = this;
         var slideShowTimer = setInterval(function() {
-            $.magnificPopup.instance.next();
+            var slidePaused = !_this._popup.data('slide_show');
+            if(slidePaused) {
+                clearInterval(slideShowTimer);
+            } else {
+                $.magnificPopup.instance.next();
+            }
         }, 3000);
         this._popup.data('slide-show-timer', slideShowTimer);
     },
@@ -191,62 +211,70 @@ var Album = {
     },
 
     _dummy: function() {
-        return [
-            {
-                src: '/static/img/stories-1.jpg',
-                type: 'image',
-                hide_title: false
-            },{
-                src: '/static/img/stories-2.jpg',
-                type: 'image',
-                hide_title: false
-            },{
-                src: '/static/img/stories-3-1.jpg',
-                type: 'image',
-                hide_title: false
-            },{
-                src: '/static/img/stories-3-2.jpg',
-                type: 'image',
-                hide_title: false
-            },{
-                src: '/static/img/stories-4.jpg',
-                type: 'image',
-                hide_title: false
-            },
-            {
-                src: '#author',
-                type: 'inline',
-                hide_title: true
-            }
-        ]
+        return {
+            slides: [
+                {
+                    src: '/static/img/stories-1.jpg',
+                    type: 'image',
+                    show_title: true
+                }, {
+                    src: '/static/img/stories-2.jpg',
+                    type: 'image',
+                    show_title: true
+                }, {
+                    src: '/static/img/stories-3-1.jpg',
+                    type: 'image',
+                    show_title: true
+                }, {
+                    src: '/static/img/stories-3-2.jpg',
+                    type: 'image',
+                    show_title: true
+                }, {
+                    src: '/static/img/stories-4.jpg',
+                    type: 'image',
+                    show_title: true
+                }],
+
+            authors: [
+                {
+                    // src: '.author',
+                    // type: 'inline',
+                    // show_title: false,
+                    name: 'name1',
+                    bio: 'bio1'
+                },
+                {
+                    // src: '.author',
+                    // type: 'inline',
+                    // show_title: false,
+                    name: 'name2',
+                    bio: 'bio2'
+                }
+            ]
+        }
     },
 
     _initImage: function() {
-        $('.btn-fullscreen').on('click', function() {
-            $('.mfp-container').addClass('mfp-container-fullscreen');
-            return false;
-        });
-
-        $('.btn-slideshow').on('click', $.proxy(function() {
-            var slideshow = this._popup.data("slideshow");
-            if(slideshow) {
-               this._popup.removeData('slideshow');
-            } else {
-                this._popup.data('slideshow', 'true');
-            }
-            slideshow = this._popup.data("slideshow");
-
-            this._updateSlideshowButtonIcon();
-            if(!this._player.isPlaying()) {
-                this._initAudio();
-            }
-
-            if(slideshow && !this._player.isPlaying()) {
-                this._toggleWidget();
-            }
-
-            return false;
-        }, this));
+        // $('.btn-slideshow').on('click', $.proxy(function() {
+        //     var slideshow = this._popup.data("slideshow");
+        //     if(slideshow) {
+        //        this._popup.removeData('slideshow');
+        //     } else {
+        //         this._popup.data('slideshow', 'true');
+        //     }
+        //     slideshow = this._popup.data("slideshow");
+        //
+        //     this._updateSlideshowButtonIcon();
+        //     if(!this._player.isPlaying()) {
+        //         this._initAudio();
+        //     }
+        //
+        //     if(slideshow && !this._player.isPlaying()) {
+        //         this._toggleWidget();
+        //     }
+        //
+        //     return false;
+        // }, this));
 
         $('.popup-info').on('click', function() {
             $('.mfp-container').toggleClass('mfp-container-fullscreen');
@@ -256,39 +284,39 @@ var Album = {
         $('.back-to-albums').on('click', function () {
            $.magnificPopup.close();
         });
-    },
-
-    _initAudio: function() {
-        var audio = $('.mfp-title .image-caption').data('audio');
-        var controls = $('.mfp-controls');
-        var slideshow = this._popup.data('slideshow');
-
-        if(audio && audio != "") {
-            slideshow ? this._initPauseButton() : this._initPlayButton();
-
-            controls.show();
-            controls.off('click');
-            controls.on('click', $.proxy(this._toggleWidget, this));
-
-            this._reloadWidget(audio, slideshow);
-        } else {
-            controls.hide();
-        }
-    },
-
-    _togglePlayButton: function() {
-        $('.audio').toggle();
-    },
-
-    _initPlayButton: function(){
-        $('.fa-play', '.mfp-controls').show();
-        $('.fa-pause', '.mfp-controls').hide();
-    },
-
-    _initPauseButton: function(){
-        $('.fa-play', '.mfp-controls').hide();
-        $('.fa-pause', '.mfp-controls').show();
     }
+
+    // _initAudio: function() {
+    //     var audio = $('.mfp-title .image-caption').data('audio');
+    //     var controls = $('.mfp-controls');
+    //     var slideshow = this._popup.data('slideshow');
+    //
+    //     if(audio && audio != "") {
+    //         slideshow ? this._initPauseButton() : this._initPlayButton();
+    //
+    //         controls.show();
+    //         controls.off('click');
+    //         controls.on('click', $.proxy(this._toggleWidget, this));
+    //
+    //         this._reloadWidget(audio, slideshow);
+    //     } else {
+    //         controls.hide();
+    //     }
+    // },
+
+    // _togglePlayButton: function() {
+    //     $('.audio').toggle();
+    // },
+    //
+    // _initPlayButton: function(){
+    //     $('.fa-play', '.mfp-controls').show();
+    //     $('.fa-pause', '.mfp-controls').hide();
+    // },
+    //
+    // _initPauseButton: function(){
+    //     $('.fa-play', '.mfp-controls').hide();
+    //     $('.fa-pause', '.mfp-controls').show();
+    // }
 };
 
 $(function() {
