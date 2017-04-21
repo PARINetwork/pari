0e015a2f0ccdcd1292e96de2199e65076c2f0e51
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-04-21 15:29:06 +0530

    #fix Compute unicode alt_text. Fixes encoding issue.

--- face/templatetags/face_filters.py
+++ face/templatetags/face_filters.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter(name='alt_text')
 def alt_text(face):
-    return '{name} is a {occupation} from {location}'.format(name=face.title, occupation=face.occupation or 'person',
+    return u'{name} is a {occupation} from {location}'.format(name=face.title, occupation=face.occupation or 'person',
                                                              location=face.location or 'India')
 
 
