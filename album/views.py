32f71a44f7e57006fefa930a7b3b4c4db1f373ed
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-05-03 17:30:46 +0530

    #88 Show state of the album location

--- album/views.py
+++ album/views.py
@@ -103,7 +103,8 @@ def get_slide_detail(request, slug):
         d = datetime.datetime.strptime(str(album.first_published_at)[:10], "%Y-%m-%d")
         date = d.strftime('%d %b,%Y')
         slide_dict['image_captured_date'] = date
-        slide_dict['slide_location'] = ",".join(slide.image.locations.all().values_list('district', flat=True))
+        image_location = slide.image.locations.first()
+        slide_dict['slide_location'] = "%s, %s" % (image_location.district, image_location.state) if image_location else ''
         slide_dict['track_id'] = slide.audio
         response_data['slides'].append(slide_dict)
 
