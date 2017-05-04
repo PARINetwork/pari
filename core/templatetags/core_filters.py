aa04977cdfb816745effa3aa4618cdaa8bebd5ec
Vidhya <svidhyalakshimi@gmail.com>
2017-05-04 13:45:44 +0530

    #92 Added audio and video type icons for article listing

--- core/templatetags/core_filters.py
+++ core/templatetags/core_filters.py
@@ -9,4 +9,10 @@ def get_type(obj):
 
     if type_ == 'album':
         return 'talking_album' if obj.slides.first().audio else 'photo_album'
+    if type_ == 'article':
+        categories = obj.categories.values_list('name', flat=True)
+        if 'VideoZone' in categories:
+            return 'video_zone'
+        if 'AudioZone' in categories:
+            return 'audio_zone'
     return type_
