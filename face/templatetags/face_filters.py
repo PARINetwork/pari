23b6bd0579745e8378a6d8ef09ae67e846538c71
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-05-08 15:19:59 +0530

    #76 Remove unused template filter

--- face/templatetags/face_filters.py
+++ face/templatetags/face_filters.py
@@ -8,11 +8,3 @@ def alt_text(face):
     return u'{name} is a {occupation} from {location}'.format(name=face.title, occupation=face.occupation or 'person',
                                                              location=face.location or 'India')
 
-
-@register.filter
-def matching_district(face, starts_with):
-    if face.location.district.lower().startswith(starts_with.lower()):
-        return face.location.district
-
-    face_image_location = face.image.locations.filter(district__istartswith=starts_with).first()
-    return face_image_location and face_image_location.district or ''
