e3b2602f0c3d0cfdaf17db64bf4e01108e1af07e
Lokesh Sharma <slokesh@thoughtworks.com>
2017-05-11 18:09:07 +0530

    Fixed guidlines content hidden issue

--- core/static/js/guidelines.js
+++ core/static/js/guidelines.js
@@ -1,8 +1,30 @@
-$(function() {
+$(function () {
+    if (!window.location.hash) {
+        window.location.hash = '#section-1-1';
+    }
+    $("a[href='" + window.location.hash + "']").addClass("current-link");
+    scrollToElement(window.location.hash);
+
+
     $("a.index-link").on('click', function () {
         $("a.index-link").each(function () {
-           $(this).removeClass('current-link');
+            $(this).removeClass('current-link');
         });
         $(this).addClass('current-link');
     });
+
+    $(".guidelines-page a[href^='#']").on('click', function (e) {
+        e.preventDefault();
+        scrollToElement(this.hash);
+        window.location.hash = this.hash
+    });
+
+    function scrollToElement(hash) {
+        $("html, body").animate({
+            scrollTop: 0
+        });
+        var parent = $('.guidelines-page .content'),
+            childToScroll = $(hash);
+        parent.scrollTop(parent.scrollTop() + childToScroll.position().top);
+    }
 });
