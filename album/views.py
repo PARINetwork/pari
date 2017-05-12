905022e8edf38b5fef1aa2c71116c7a551ec55bf
Deepthi R <deepthirera@gmail.com>
2017-05-12 16:06:38 +0530

    [Dee] Converted the description to richtext content

--- album/views.py
+++ album/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.core.cache import cache
 from django.http import JsonResponse
 
+from wagtail.wagtailcore.rich_text import RichText
+from wagtail.wagtailcore import blocks
 from wagtail.wagtailadmin.modal_workflow import render_modal_workflow
 
 from album.models import Album, AlbumSlide
@@ -92,7 +94,9 @@ def get_slide_detail(request, slug):
         slide_dict = dict([('type', 'image'), ('show_title', "True"), ('album_title', album.title)])
         slide_dict['src'] = slide.image.file.url
         slide_dict['src_resized'] = slide.image.get_rendition('height-876').url
-        slide_dict['description'] = slide.description
+        block = blocks.RichTextBlock()
+        description_value = RichText(slide.description)
+        slide_dict['description'] = block.render(description_value)
         slide_dict['album_description'] = album.description
         slide_dict['url'] = request.build_absolute_uri().replace(".json", "")
         slide_dict['slide_photographer'] = map(lambda photographer_name: photographer_name.name.encode('UTF-8'),
