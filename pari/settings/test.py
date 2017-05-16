f471f2d5653cf469a94972761470ca01851a4854
Deepthi R <deepthirera@gmail.com>
2017-05-16 15:46:37 +0530

    #101 [Dee/Lok] Test setting for separate db,sever while running functional tests

---
+++ pari/settings/test.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test_pari',
+        'USER': 'pari',
+        'PASSWORD': 'pari',
+        'HOST': 'localhost',  # Set to empty string for localhost.
+        'PORT': '',  # Set to empty string for default.
+        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+        'TEST': {
+            'NAME': 'test_pari',
+        },
+    }
+}
