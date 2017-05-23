5f05ed5e3b5b60fac50bd1075a0ff99545120d44
Deepthi R <deepthirera@gmail.com>
2017-05-23 15:43:59 +0530

    #101 [Dee] home page  in focus test

--- functional_tests/pages/home_page.py
+++ functional_tests/pages/home_page.py
@@ -13,6 +13,8 @@ class HomePage(Page):
     def all_that_you_can_discover_on_pari_section(self):
         return AllThatYouCanDiscoverOnPARISection(self.driver)
 
+    def in_focus_section(self):
+        return InFocusSection(self.driver)
 
 class AllThatYouCanDiscoverOnPARISection():
     def __init__(self, driver):
@@ -49,3 +51,31 @@ class AllThatYouCanDiscoverOnPARISection():
     def sub_text_of_fourth_category(self):
         return self.driver.find_element(By.XPATH,
                                         "//div[@class='col-lg-6 col-md-6 col-sm-12 col-xs-12 grid category-3']//div[@class='title-grid']/div[2]").text
+
+class InFocusSection():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def title_of_first_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page1']//div[@class='infocus-item-title']").text
+
+    def authors_of_first_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page1']//div[@class='authors']").text
+
+    def date_of_first_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page1']//span[@class='location-name']").text
+
+    def location_of_first_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page1']//span[@class='date-infocus']").text
+    
+    def title_of_second_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page2']//div[@class='infocus-item-title']").text
+
+    def authors_of_second_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page2']//div[@class='authors']").text
+
+    def date_of_second_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page2']//span[@class='location-name']").text
+
+    def location_of_second_article(self):
+        return self.driver.find_element(By.XPATH, "//div[@class='row in_focus_page2']//span[@class='date-infocus']").text
--- functional_tests/tests/test_home_page.py
+++ functional_tests/tests/test_home_page.py
@@ -13,17 +13,19 @@ class TestHomePage(Test):
     @classmethod
     def setUpClass(cls):
         super(TestHomePage, cls).setUpClass()
-        author = AuthorFactory.create()
+        author1 = AuthorFactory.create()
+        author2 = AuthorFactory.create(name="Karthik", slug="srk")
         category1 = CategoryFactory.create(name="Resource Conflicts", slug="resource-conflicts", order=1, description="Jal, jungle, zameen")
         category2 = CategoryFactory.create(name="Adivasis", slug="adivasis", order=2, description="The first dwellers")
         category3 = CategoryFactory.create(name="Dalits", slug="dalits", order=3, description="Struggles of the oppressed")
         category4 = CategoryFactory.create(name="Rural Sports", slug="sports-games", order=4, description="Games people play")
-        location = LocationFactory.create()
-        image = ImageFactory.create(photographers=(author,), locations=(location,))
+        location1 = LocationFactory.create()
+        location2 = LocationFactory.create(name="Madurai", slug="madurai")
+        image = ImageFactory.create(photographers=(author1,), locations=(location1,))
         setup = DataSetup()
-        article1 = setup.create_article("article1", author, category1, location, image)
-        article2 = setup.create_article("article2", author, category2, location, image)
-        video_article = setup.create_video_article("video article", author, location, image)
+        article1 = setup.create_article("article1", author1, category1, location1, image)
+        article2 = setup.create_article("article2", author2, category2, location2, image)
+        video_article = setup.create_video_article("video article", author1, location1, image)
         talking_album = setup.create_talking_album(image)
         photo_album = setup.create_photo_album(image)
         HomePageFactory.create(carousel_0=article1, carousel_1=article2, in_focus_page1=article1, in_focus_page2=article2, video=video_article, talking_album=talking_album, photo_album=photo_album)
@@ -36,21 +38,35 @@ class TestHomePage(Test):
         assert self.home_page.title == "People's Archive of Rural India"
 
     def test_first_category_of_all_that_you_can_discover_on_pari_section(self):
-        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section
-        assert discover_on_pari_section().title_of_first_category() == "Resource Conflicts"
-        assert discover_on_pari_section().sub_text_of_first_category() == "Jal, jungle, zameen"
+        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section()
+        assert discover_on_pari_section.title_of_first_category() == "Resource Conflicts"
+        assert discover_on_pari_section.sub_text_of_first_category() == "Jal, jungle, zameen"
 
     def test_second_category_of_all_that_you_can_discover_on_pari_section(self):
-        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section
-        assert discover_on_pari_section().title_of_second_category() == "Adivasis"
-        assert discover_on_pari_section().sub_text_of_second_category() == "The first dwellers"
+        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section()
+        assert discover_on_pari_section.title_of_second_category() == "Adivasis"
+        assert discover_on_pari_section.sub_text_of_second_category() == "The first dwellers"
 
     def test_third_category_of_all_that_you_can_discover_on_pari_section(self):
-        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section
-        assert discover_on_pari_section().title_of_third_category() == "Dalits"
-        assert discover_on_pari_section().sub_text_of_third_category() == "Struggles of the oppressed"
+        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section()
+        assert discover_on_pari_section.title_of_third_category() == "Dalits"
+        assert discover_on_pari_section.sub_text_of_third_category() == "Struggles of the oppressed"
 
     def test_fourth_category_of_all_that_you_can_discover_on_pari_section(self):
-        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section
-        assert discover_on_pari_section().title_of_fourth_category() == "Rural Sports"
-        assert discover_on_pari_section().sub_text_of_fourth_category() == "Games people play"
+        discover_on_pari_section = self.home_page.all_that_you_can_discover_on_pari_section()
+        assert discover_on_pari_section.title_of_fourth_category() == "Rural Sports"
+        assert discover_on_pari_section.sub_text_of_fourth_category() == "Games people play"
+
+    def test_in_focus_page_one(self):
+        in_focus_section = self.home_page.in_focus_section()
+        assert in_focus_section.title_of_first_article() == "article1"
+        assert in_focus_section.authors_of_first_article() == "V. Sasikumar"
+        assert in_focus_section.date_of_first_article() == "Sivaganga"
+        assert in_focus_section.location_of_first_article() == "October 24, 2011"
+
+    def test_in_focus_page_two(self):
+        in_focus_section = self.home_page.in_focus_section()
+        assert in_focus_section.title_of_second_article() == "article2"
+        assert in_focus_section.authors_of_second_article() == "Karthik"
+        assert in_focus_section.date_of_second_article() == "Madurai"
+        assert in_focus_section.location_of_second_article() == "October 24, 2011"
