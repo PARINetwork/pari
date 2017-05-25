e8150ae0d6cd660f9cee971a1da79e2bd7ceb9f9
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-05-25 14:50:56 +0530

    #101 [Sankar/Lokesh] Mod Travis yaml and added test.sh for functional test

---
+++ scripts/test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -oe
+
+echo "Running Migrations...."
+python manage.py migrate --settings=pari.settings.test
+
+echo "Running Collectstatic...."
+python manage.py collectstatic --noinput
+
+echo "Starting Server..........."
+python manage.py runserver --settings=pari.settings.test > /dev/null 2>&1 &
+
+expected_response=200
+end=$((SECONDS+60))
+
+echo "Creating media uploads........."
+mkdir -p "media/uploads/"
+cp "core/static/img/stories-1.jpg" "media/uploads/stories-1.jpg"
+
+while [ "$SECONDS" -lt "$end" ];
+do
+  sleep 2
+  response=`curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/pages/donate/`
+
+  echo "Waiting for service to start...."
+  if [ "$response" == "$expected_response" ]
+  then
+    is_service_started='true'
+    echo "Service Started........."
+    break
+  fi
+done
+if [ "$is_service_started" != 'true' ];
+then
+   echo "Unable to start the service........"
+   exit 1
+fi
+
+echo "Running test......."
+python manage.py test --settings=pari.settings.test -k -v 3
