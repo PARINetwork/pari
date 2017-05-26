487db91f7f6bc745f82a53022d010a8faea7169d
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-05-26 18:56:36 +0530

    #104 Add custom face chooser block

--- article/streamfields/blocks.py
+++ article/streamfields/blocks.py
@@ -1,4 +1,7 @@
+from django.utils.functional import cached_property
+
 from wagtail.wagtailadmin import blocks
+from wagtail.wagtailcore.blocks import PageChooserBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
@@ -9,6 +12,28 @@ class ImageBlock(blocks.StructBlock):
     class Meta:
         icon = 'image'
         template = 'article/blocks/image.html'
+
+
+class FaceChooserBlock(PageChooserBlock):
+    def __init__(self, can_choose_root=False, **kwargs):
+        super(FaceChooserBlock, self).__init__(can_choose_root=can_choose_root, **kwargs)
+
+    @cached_property
+    def target_model(self):
+        from face.models import Face
+        return Face
+
+    @cached_property
+    def widget(self):
+        from django.utils.translation import ugettext_lazy as _
+        from wagtail.wagtailadmin.widgets import AdminPageChooser
+        from face.models import Face
+
+        admin_face_chooser = AdminPageChooser(target_models=[Face], can_choose_root=self.can_choose_root)
+        admin_face_chooser.choose_one_text = _('Choose a face')
+        admin_face_chooser.choose_another_text = _('Choose another face')
+        admin_face_chooser.link_to_chosen_text = _('Edit this face')
+        return admin_face_chooser
 
 
 class FullWidthImageBlock(blocks.StructBlock):
