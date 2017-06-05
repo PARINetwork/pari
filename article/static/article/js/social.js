f434603f3502f509852c118a80615c682c202824
Lokesh Sharma <slokesh@thoughtworks.com>
2017-06-05 18:54:31 +0530

    Added scroll for the banner info in article

--- article/static/article/js/social.js
+++ article/static/article/js/social.js
@@ -1,6 +1,14 @@
-$(function(){
+$(function () {
     if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-	// Mobile device.
-	$(".hidden.whatsapp").removeClass("hidden")
+        // Mobile device.
+        $(".hidden.whatsapp").removeClass("hidden")
     }
+
+    $('.glyphicon-chevron-up').onclick(function () {
+        $(".arrow-collapse").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+    });
+
+    $('.glyphicon-chevron-down').onClick( function () {
+        $(".arrow-collapse").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+    });
 });
