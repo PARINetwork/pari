302fdecc7b3b6f308af36a5578ea179c68b623aa
Lokesh Sharma <slokesh@thoughtworks.com>
2017-06-08 11:51:23 +0530

    Revert "Revert "#115 Added checkbox for the cover image for the article""
    This reverts commit 8847bb735fc450c5f91d23146ade88a972a836bb.

--- article/static/article/js/social.js
+++ article/static/article/js/social.js
@@ -3,4 +3,15 @@ $(function(){
 	// Mobile device.
 	$(".hidden.whatsapp").removeClass("hidden")
     }
+
+
+    $('.arrow-wrap').on("click", function () {
+        if ($(".arrow-collapse").hasClass("glyphicon-chevron-up"))
+        {
+            $(".arrow-collapse").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+        }else
+        {
+            $(".arrow-collapse").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        }
+    });
 });
