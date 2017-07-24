b562cf74d7544a13593931aa927619dfc0171185
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-07-24 16:45:09 +0530

    Jayakumar/Sankar #149 Merge modular-content migrations to single file

--- article/migrations/0009_add_stream_fields_for_modular_article_content.py
+++ article/migrations/0009_add_stream_fields_for_modular_article_content.py
@@ -12,11 +12,11 @@ import article.streamfields.blocks
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('article', '0010_article_modular_content_paragraph_with_quote'),
+        ('article', '0008_made_date_fields_selectable'),
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='article',
             name='modular_content',
             field=wagtail.wagtailcore.fields.StreamField([('full_width_image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.CharBlock(required=False))]))])), ('two_column_image', wagtail.wagtailcore.blocks.StructBlock([(b'image_left', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.CharBlock(required=False))])), (b'image_right', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.CharBlock(required=False))]))])), ('paragraph', wagtail.wagtailcore.blocks.StructBlock([(b'content', wagtail.wagtailcore.blocks.RichTextBlock())])), ('paragraph_with_image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.CharBlock(required=False))])), (b'align_image', wagtail.wagtailcore.blocks.ChoiceBlock(default=b'left', choices=[(b'left', b'Left'), (b'right', b'Right')])), (b'content', wagtail.wagtailcore.blocks.StructBlock([(b'content', wagtail.wagtailcore.blocks.RichTextBlock())]))])), ('face', wagtail.wagtailcore.blocks.StructBlock([(b'face', article.streamfields.blocks.PageTypeChooserBlock(for_models=[face.models.Face]))])), ('paragraph_with_block_quote', wagtail.wagtailcore.blocks.StructBlock([(b'quote', article.streamfields.blocks.CustomRichTextBlock(buttons=[[[b'bold', b'italic'], [b'undo', b'redo']]], editor=b'tinymce')), (b'align_quote', wagtail.wagtailcore.blocks.ChoiceBlock(default=b'right', choices=[(b'left', b'Left'), (b'right', b'Right')])), (b'paragraph', wagtail.wagtailcore.blocks.RichTextBlock())]))], null=True, blank=True),
