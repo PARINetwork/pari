a364593948b98896da4af495872ec524a1d3e5a1
Lokesh Sharma <slokesh@thoughtworks.com>
2017-07-28 16:30:09 +0530

    #142 [Lokesh/Nimesh] Made partial search for location

--- article/models.py
+++ article/models.py
@@ -107,7 +107,7 @@ class Article(Page):
         index.SearchField('content', partial_match=True, boost=SearchBoost.CONTENT),
         index.SearchField('modular_content', partial_match=True, boost=SearchBoost.CONTENT),
         index.SearchField('get_district_from_location', partial_match=True, boost=SearchBoost.LOCATION),
-        index.SearchField('language'),
+        index.SearchField('language', partial_match=True),
         index.FilterField('categories'),
         index.FilterField('language'),
         index.FilterField('get_search_type'),
