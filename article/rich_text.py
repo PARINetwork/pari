5f5322f2824216b9bb4ea7b568ea760ff03634eb
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-08-01 18:16:39 +0530

    #149 Refactor configurable hallo text editor

--- article/rich_text.py
+++ article/rich_text.py
@@ -3,13 +3,11 @@ from __future__ import absolute_import, unicode_literals
 import json
 
 from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.forms import Media, widgets
-from wagtail.utils.widgets import WidgetWithScript
-from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
-from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
+from django.forms import Media
+from wagtail.wagtailadmin.rich_text import HalloRichTextArea
 
 
-class CustomHalloRichTextArea(WidgetWithScript, widgets.Textarea):
+class CustomHalloRichTextArea(HalloRichTextArea):
     @classmethod
     def get_default_options(cls):
         return {
@@ -30,24 +28,8 @@ class CustomHalloRichTextArea(WidgetWithScript, widgets.Textarea):
         if kwargs:
             self.kwargs.update(kwargs)
 
-    def get_panel(self):
-        return RichTextFieldPanel
-
-    def render(self, name, value, attrs=None):
-        if value is None:
-            translated_value = None
-        else:
-            translated_value = expand_db_html(value, for_editor=True)
-        return super(CustomHalloRichTextArea, self).render(name, translated_value, attrs)
-
     def render_js_init(self, id_, name, value):
         return "makeCustomHalloRichTextEditable({0}, {1});".format(json.dumps(id_), json.dumps(self.kwargs))
-
-    def value_from_datadict(self, data, files, name):
-        original_value = super(CustomHalloRichTextArea, self).value_from_datadict(data, files, name)
-        if original_value is None:
-            return None
-        return DbWhitelister.clean(original_value)
 
     @property
     def media(self):
