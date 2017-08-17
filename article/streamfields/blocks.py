0f990a0c12fbfd87d0bf67a97b76c6882d967c05
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-08-17 17:42:12 +0530

    #164 Add the ability to align map widget

--- article/streamfields/blocks.py
+++ article/streamfields/blocks.py
@@ -254,8 +254,10 @@ class VideoWithQuoteBlock(blocks.StructBlock):
 
 
 class ParagraphWithMapBlock(blocks.StructBlock):
+    ALIGN_MAP_CHOICES = [('left', 'Left'), ('right', 'Right')]
     content = ParagraphBlock()
     locations = ModelMultipleChoiceBlock(target_model=Location)
+    map_align = blocks.ChoiceBlock(choices=ALIGN_MAP_CHOICES, default=ALIGN_MAP_CHOICES[0][0])
 
     class Meta:
         label = 'Paragraphs with map'
