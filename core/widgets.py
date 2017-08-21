411c89a126589c8b5b9aa2a72c53a8387f372d15
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-08-21 11:55:37 +0530

    #164 Add JqueryChosen widget with the ability to add object

--- core/widgets.py
+++ core/widgets.py
@@ -1,4 +1,5 @@
 from django.forms import SelectMultiple
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 
@@ -23,4 +24,23 @@ class JqueryChosenSelectMultiple(SelectMultiple):
 
         js = (
             'js/chosen.jquery.min.js',
+        )
+
+
+class JqueryChosenAddModel(JqueryChosenSelectMultiple):
+    def render(self, name, value, attrs=None, choices=()):
+        out = super(JqueryChosenAddModel, self).render(name, value, attrs, choices)
+
+        field_name = name.split('-')[-1]
+        model_add_button = format_html('''
+        <div class="addbutton">
+            <a href="javascript:void(0);" onclick="addObject('{name}', '#id_{name}');"
+            class="button bicolor icon icon-plus">Add {name}</a>
+        </div>''', name=field_name)
+
+        return mark_safe(out + model_add_button)
+
+    class Media:
+        js = (
+            'js/add_object.js',
         )
