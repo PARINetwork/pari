921a5671d3d8a67b6a417a295792210803900d57
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-09-20 12:44:45 +0530

    Jayakumar/Lokesh Workaround to make travis wait for longer time before aborting deployment

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-chmod 400 pari_qa.pem
-#todo find a elegant way to instruct ansible to take right pem
-cp pari_qa.pem ~/.ssh/id_rsa
-git clone https://github.com/PARINetwork/pari-ansible.git
-cd pari-ansible
-ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml
