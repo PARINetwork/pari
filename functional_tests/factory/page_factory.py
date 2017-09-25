c9636d74475b262ceb880bb4ba374704eb01f9b1
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-09-25 12:14:48 +0530

    Change expiry date for seed data articles

--- functional_tests/factory/page_factory.py
+++ functional_tests/factory/page_factory.py
@@ -29,7 +29,7 @@ class PageFactory(factory.django.DjangoModelFactory):
     show_in_menus = False
     search_description = " "
     go_live_at = '2011-10-24 12:43'
-    expire_at = '2011-10-24 12:43'
+    expire_at = '2050-12-31 12:43'
     expired = False
     content_type = factory.SubFactory(ContentTypeFactory)
     locked = False
