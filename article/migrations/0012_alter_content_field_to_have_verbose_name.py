4d2a513794ab61c1a6264807daaac9946cfebd25
Jayakumar Chinnappan <jayakumareee21@gmail.com>
2017-09-26 16:43:45 +0530

    Add migration for article content field verbose name introduction

---
+++ article/migrations/0012_alter_content_field_to_have_verbose_name.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import wagtail.wagtailcore.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('article', '0011_alter_content_and_modular_content_fields'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='article',
+            name='content',
+            field=wagtail.wagtailcore.fields.RichTextField(verbose_name="Content - Deprecated. Use 'MODULAR CONTENT' instead.", blank=True),
+        ),
+    ]
