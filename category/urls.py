78f50f9c4da690a63d5d0afd80734a867bfddd0c
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-10-12 15:38:22 +0530

    SEO optimization for url endpoints

--- category/urls.py
+++ category/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, url
 from .views import CategoriesList, GalleryDetail, StoryDetail
 
 urlpatterns = patterns('category.views',
-    url(r'^categories/$', CategoriesList.as_view(), name='category-list'),
-    url(r'^categories/(?P<slug>.+)/$', StoryDetail.as_view(), name='category-detail'),
-    url(r'^stories/categories/(?P<slug>.+)/$', StoryDetail.as_view(), name='story-detail'),
-    url(r'^gallery/categories/(?P<slug>.+)/$', GalleryDetail.as_view(), name='gallery-detail'),
+    url(r'^categories/?$', CategoriesList.as_view(), name='category-list'),
+    url(r'^categories/(?P<slug>.+)/?$', StoryDetail.as_view(), name='category-detail'),
+    url(r'^stories/categories/(?P<slug>.+)/?$', StoryDetail.as_view(), name='story-detail'),
+    url(r'^gallery/categories/(?P<slug>.+)/?$', GalleryDetail.as_view(), name='gallery-detail'),
 
-    url(r'^gallery/$', 'gallery_home_page', name='gallery-home-page')
+    url(r'^gallery/?$', 'gallery_home_page', name='gallery-home-page')
 )
--- feeds/urls.py
+++ feeds/urls.py
@@ -3,10 +3,10 @@ from .feeds import AllFeed, ArticleFeed, AlbumFeed, \
     FaceFeed, ResourceFeed
 
 urlpatterns = patterns('',
-    url(r'^list/$', 'feeds.feeds.feeds_list_page', name="feeds_list_page"),
-    url(r'^all/$', AllFeed(), name="all_feeds"),
-    url(r'^articles/$', ArticleFeed(), name="article_feeds"),
-    url(r'^albums/$', AlbumFeed(), name="album_feeds"),
-    url(r'^faces/$', FaceFeed(), name="face_feeds"),
-    url(r'^resources/$', ResourceFeed(), name="resource_feeds"),
+    url(r'^list/?$', 'feeds.feeds.feeds_list_page', name="feeds_list_page"),
+    url(r'^all/?$', AllFeed(), name="all_feeds"),
+    url(r'^articles/?$', ArticleFeed(), name="article_feeds"),
+    url(r'^albums/?$', AlbumFeed(), name="album_feeds"),
+    url(r'^faces/?$', FaceFeed(), name="face_feeds"),
+    url(r'^resources/?$', ResourceFeed(), name="resource_feeds"),
 )
