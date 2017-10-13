0f341e497d89a526d04b67503e0fc786ad3775bf
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-10-13 16:52:35 +0530

    SEO url Optimzation for album

--- album/urls.py
+++ album/urls.py
@@ -5,9 +5,9 @@ from album.views import AlbumDetail, AlbumList, get_slide_detail
 
 urlpatterns = patterns('album.views',
         url(r'^$', AlbumList.as_view(), {'filter': 'all'}, name='album-list'),
-        url(r'^talking/$', AlbumList.as_view(), {'filter': 'talking'}, name='talking-album-list'),
-        url(r'^other/$', AlbumList.as_view(), {'filter': 'other'}, name='other-album-list'),
-        url(r'^(?P<slug>.+)/all/$', AlbumDetail.as_view(), name='image-collection-image-list'),
+        url(r'^talking/?$', AlbumList.as_view(), {'filter': 'talking'}, name='talking-album-list'),
+        url(r'^other/?$', AlbumList.as_view(), {'filter': 'other'}, name='other-album-list'),
+        url(r'^(?P<slug>.+)/all/?$', AlbumDetail.as_view(), name='image-collection-image-list'),
         url(r'^(?P<slug>.+).json/$', get_slide_detail, name='album_slide_detail'),
         url(r'^(?P<slug>.+)/$', AlbumDetail.as_view(), name='album-detail'),
 )
