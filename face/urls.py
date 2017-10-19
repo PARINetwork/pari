07816a316e24c8c5c0564786b3125c1093970e94
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-10-19 13:20:16 +0530

    SEO for faces url

--- face/urls.py
+++ face/urls.py
@@ -5,10 +5,10 @@ from django.views.generic import RedirectView
 from .views import FaceList, FaceDetail
 
 urlpatterns = patterns('faces.views',
-                       url(r'^faces/$', RedirectView.as_view(url='/categories/faces/')),
-                       url(r'^categories/faces/$', FaceList.as_view(), name='face-list'),
-                       url(r'^categories/faces/(?P<alphabet>\w)/$', FaceDetail.as_view(),
+                       url(r'^faces/?$', RedirectView.as_view(url='/categories/faces/')),
+                       url(r'^categories/faces/?$', FaceList.as_view(), name='face-list'),
+                       url(r'^categories/faces/(?P<alphabet>\w)/?$', FaceDetail.as_view(),
                            name='face-detail'),
-                       url(r'^categories/faces/(?P<alphabet>\w)/(?P<slug>[\w\-]+)/$', FaceDetail.as_view(),
+                       url(r'^categories/faces/(?P<alphabet>\w)/(?P<slug>[\w\-]+)/?$', FaceDetail.as_view(),
                            name='face-detail-single'),
                        )
