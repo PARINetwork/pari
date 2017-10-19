1263502d9d5bcf042ca25c766f3d682211ab173c
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-10-19 11:00:12 +0530

    Revert "SEO url for articles"
    This reverts commit 5cd459fc7fe9d27116ca9f7366b8d4dfab1ed91c.

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from location.views import LocationDetail
 
 urlpatterns = patterns('article.views',
     url(r'^authors/(?P<slug>.+)/$', AuthorArticleList.as_view(), name='author-detail'),
-    url(r'^articles/?$', ArticleList.as_view(), {'filter': 'article-list'}, name='article-list'),
+    url(r'^articles/$', ArticleList.as_view(), {'filter': 'article-list'}, name='article-list'),
     url(r'^articles/(?P<slug>.+)/$', ArticleDetail.as_view(), name='article-detail'),
     url(r'^locations/(?P<slug>.+)/$', LocationDetail.as_view(), name='location-detail'),
     url(r'^archive/(?P<year>\d{4})/(?P<month>\d+)/$', ArchiveDetail.as_view(), name='archive-detail'),
