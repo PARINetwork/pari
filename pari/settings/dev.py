005bc39f383dc2192d2c18905f6bf17e370607fa
Nikhil Sane <nikhil.sane@gmail.com>
2018-02-25 23:18:51 -0800

    configuration changes for logging

--- pari/settings/dev.py
+++ pari/settings/dev.py
@@ -20,16 +20,36 @@ NPLUSONE_LOG_LEVEL = logging.WARN
 
 LOGGING = {
     'version': 1,
+    'disable_existing_logger': False,
+    'formatters':{
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'filename': '/vagrant/logs/pari.log',
+            'maxBytes': 1024 * 1024 * 10, # 10MB,
+            'backupCount': 20,
+            'formatter': 'verbose'
+        }
     },
     'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
         'nplusone': {
             'handlers': ['console'],
             'level': 'WARN',
-        },
+        }
     },
 }
 
--- pari/settings/staging.py
+++ pari/settings/staging.py
@@ -14,6 +14,37 @@ ALLOWED_HOSTS = [u'staging.ruralindiaonline.org']
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_logger': False,
+    'formatters':{
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'filename': '/vagrant/logs/pari.log',
+            'maxBytes': 1024 * 1024 * 10, # 10MB,
+            'backupCount': 20,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    },
+}
+
 try:
     from .local import *
 except ImportError:
