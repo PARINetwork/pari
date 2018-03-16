2cf81c030edc11a365a6f9af30f6c97f39218312
raunaqgupta <mail@raunaqgupta.com>
2018-03-16 15:43:52 +0530

    Update resources url redirection in case of an absent trailing slash

--- resources/urls.py
+++ resources/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import ResourceDetail, ResourceList, ReportDetail
 
 urlpatterns = patterns('',
-                       url(r'^$', ResourceList.as_view(), name='resource-list'),
-                       url(r'^(?P<slug>.+)/$', ResourceDetail.as_view(), name='resource-detail'),
+                       url(r'^resources/?$', ResourceList.as_view(), name='resource-list'),
+                       url(r'^resources/(?P<slug>.+)/$', ResourceDetail.as_view(), name='resource-detail'),
                        url(r'^(?P<slug>.+)/report$', ReportDetail.as_view(), name='report-detail')
 )
