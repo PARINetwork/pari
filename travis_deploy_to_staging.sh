70fbe065fbd29b237efe605a830956f58e8972ce
Sankar Parameswaran <sankarpa@thoughtworks.com>
2018-08-07 20:09:21 +0530

    [sankar] fix staging deploy from travis

--- travis_deploy_to_staging.sh
+++ travis_deploy_to_staging.sh
@@ -8,7 +8,7 @@ while true; do echo "Deploying..."; sleep 60; done &
 # Deployment steps
 chmod 400 pari_qa.pem
 #todo find a elegant way to instruct ansible to take right pem
-cp pari_qa.pem ~/.ssh/id_rsa
+cp pari_qa ~/.ssh/id_rsa
 git clone https://github.com/PARINetwork/pari-ansible.git
 cd pari-ansible
 ansible-playbook -l staging -u ubuntu -i hosts.yml -t deploy site.yml
