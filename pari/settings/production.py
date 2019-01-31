be6e8e42eac64a76a6fc9fe29f13804905056591
Charles <charlesj@gmail.com>
2019-01-31 17:41:44 +0530

    Add log configuration to prod settings

--- pari/settings/production.py
+++ pari/settings/production.py
@@ -35,6 +35,46 @@ try:
 except ImportError:
     pass
 
+import os
+
+PARI_LOGLEVEL = os.environ.get('PARI_LOGLEVEL', 'info').upper()
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+            'datefmt': '%H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': PARI_LOGLEVEL,
+            'class': 'logging.FileHandler',
+            'filename': os.environ.get('DJANGO_LOGFILE', '/var/log/pari-django.log'),
+            'formatter': 'console'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'pari': {
+            'level': PARI_LOGLEVEL,
+            'propagate': True,
+        },
+    }
+}
+
 sentry_sdk.init(
     dsn="https://e87c857ac5c248e5b4868cfdd71a2cbf@sentry.io/1377604",
     environment='Prod',
