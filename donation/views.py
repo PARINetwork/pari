b6439ea0aae91211cf2cbaf49f3e666e1c541f42
Vinod Deolal <vinoddeolal@gmail.com>
2021-02-18 14:34:00 +0530

    Revert "fix the base64 encoding and decoding"
    This reverts commit fa5967c3f9f3f3cf7ce2b38abf363ce492c2fa78.

--- donation/views.py
+++ donation/views.py
@@ -121,8 +121,7 @@ def handle_razorpay_payment(form_data):
         'timestamp': time.time()
     }
     checkout_url = reverse('razorpay_checkout')
-    encoded_str = json.dumps(params).encode("ascii")
-    checkout_url += "?params={0}".format(base64.b64encode(encoded_str).decode("ascii"))
+    checkout_url += "?params={0}".format(base64.b64encode(json.dumps(params)))
     return redirect(checkout_url)
 
 
@@ -196,7 +195,9 @@ def razorpay_checkout(request):
     params = request.GET.get('params')
     if not params:
         return HttpResponseBadRequest()
-    checkout_params = json.loads(base64.b64decode(params.encode("ascii")).decode('ascii'))
+
+    checkout_params = json.loads(base64.b64decode(params))
+
     current_timestamp, max_timestamp_delay = time.time(), 5.0
     if current_timestamp - checkout_params['timestamp'] > max_timestamp_delay:
         return HttpResponse("Link expired")
@@ -209,7 +210,7 @@ def razorpay_checkout(request):
                 .format(checkout_params['subscription_id']))
 
     return render(request, 'donation/razorpay_checkout.html', {
-        'checkout_params': base64.b64decode(params.encode("ascii")).decode('ascii'),
+        'checkout_params': base64.b64decode(params),
         'subscription_id': checkout_params['subscription_id']
     })
 
