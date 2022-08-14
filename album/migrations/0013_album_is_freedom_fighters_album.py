db201f035093faadc00a0253a5244db9b5ecf158
Shashikanth <shashikanth@swecha.net>
2022-08-14 12:57:55 +0530

    Modifying ff album detail page

--- album/migrations/0013_album_is_freedom_fighters_album.py
+++ album/migrations/0013_album_is_freedom_fighters_album.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('album', '0015_auto_20220512_1519'),
+        ('album', '0012_auto_20220512_1543'),
     ]
 
     operations = [
